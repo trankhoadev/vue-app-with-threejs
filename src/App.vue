8674ab9545967d0b93a2497b9eb41a1e12ee76e5
trankhoadev <trankhoadev@gmail.com>
2022-07-15 14:08:33 +0700

    Create blank project with vue and threejs setup

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <ThreeJs></ThreeJs>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+
+<script>
+  import ThreeJs from './components/ThreeJs.vue'
+  export default {
+    name: 'App',
+    components: {
+      ThreeJs
+    },
+    setup() {
+      return {}
+    },
+    data() {
+      return {}
+    }
+  }
+</script>
